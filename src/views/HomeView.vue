2cb038ab20499ceebf3a19eb13cbbe88045ba5fe
João <jv-oliveira07@hotmail.com>
2022-10-27 15:23:49 -0300

    Iniciando a HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,93 @@
 <template>
-  <h2>Tenha um tratamento individualizado para sua queda capilar.</h2>
+
+  <div class="flex column items-center q-gutter-md">
+    <h2>Tenha um tratamento individualizado para sua queda capilar.</h2>
+    <p>Assista o vídeo para entender como funciona a consulta e o tratamento:</p>
+    <p>Quero agendar:</p>
+    <p>Aguardando Vídeo</p>
+    <button>Consulta Online</button>
+    <button>Consulta Presencial</button>
+  </div>
+
+  <section class="flex column items-center q-gutter-md">
+    <h2>Se você...</h2>
+    <p>Está passando por uma queda de cabelo acentuada</p>
+    <p>Sente que seu cabelo está ficando fino e o couro cabeludo aparente</p>
+    <p>Nota que seu cabelo está sumindo aos poucos</p>
+    <p>Já fez diversos tratamentos e não teve resultado</p>
+    <p>Sente que seu cabelo está frágil e quebradiço</p>
+    <h2>Agende uma consulta capilar agora mesmo</h2>
+    <button>Consulta Online</button>
+    <button>Consulta Presencial</button>
+  </section>
+
+  <section class="flex column items-center q-gutter-md">
+      <h4>Resultados dos tratamentos capilares realizados, tanto presencialmente quanto de forma online:</h4>
+      <p>Aguardando fotos</p>
+  </section>
+
+  <section class="flex column items-center q-gutter-md">
+    <h4>Por que buscar uma consulta capilar?</h4>
+    <h6>Existem vários tipos de queda de cabelo</h6>
+    <p>Queda de cabelo não é tudo igual. Existem vários tipos de alopecias e é preciso identificar qual é a sua, para então tratar da maneira correta.</p>
+    <p>Todos os dias eu recebo pacientes que se queixam por já terem tomado várias vitaminas, usado tônicos variados e não tiveram resultado.</p>
+    <p>Só resolvem buscar ajuda profissional quando o quadro já está avançado.</p>
+    <p>A minha pergunta é: </p>
+    <h5>Você vai esperar chegar até que ponto para procurar um profissional qualificado para te ajudar?</h5>
+    <h5>TEMPO É CABELO</h5>
+    <p>Há casos em que o cabelo perdido não pode ser recuperado. Quanto antes iniciar o tratamento correto, melhores são as chances de um bom resultado.</p>
+    <p>Coloque na ponta do lápis quanto você já gastou ou pensou em gastar com shampoos antiqueda que não funcionam, tônicos caseiros sem embasamento científico algum, e até produtos importados que prometiam resultados milagrosos.</p>
+    <p>E o que aconteceu?</p>
+    <h4>Você perdeu dinheiro, tempo... e cabelo!</h4>
+    <p>É muito mais econômico que você agende primeiro uma consulta profissional, para investir no tratamento que é realmente indicado e efetivo para o seu caso.</p>
+  </section>
+
+  <section class="flex column items-center q-gutter-md">
+    <h4>Como funciona a consulta presencial?</h4>
+    <p>A consulta presencial dura cerca de uma hora e acontece perante horário agendado, no seguinte endereço:</p>
+    <p>Av. Roberto Silveira, 215 - Icaraí Av. Roberto Silveira, 215 - Icaraí, Niterói - RJ, 24230-151</p>
+    <p>Ela se divide em 3 partes principais:</p>
+    <div>
+      <h6>1 <br> Coleta de Informações</h6>
+      <p>Após agendar seu horário, o paciente recebe um documento, via WhatsApp, com orientações de como se preparar para a consulta: fotos específicas e um exame físico simples que ele faz sozinho.</p>
+      <p>As fotos devem ser enviadas previamente, junto com exames laboratoriais recentes e outros documentos importantes, se houver.</p>
+      <p>O pagamento (Pix ou Cartão de Crédito) é feito nessa etapa.</p>
+    </div>
+    <div>
+      <h6>2 <br> A Consulta</h6>
+      <p>Durante a chamada de vídeo, várias perguntas são feitas ao paciente a fim de conhecer seu histórico referente a queixa, todo o caso clínico e qualquer outra informação relevante para identificação do tipo de queda.</p>
+      <p>Ao final da consulta, será explicado todo o caso ao paciente e o tratamento será proposto e orientado.</p>
+    </div>
+    <div>
+      <h6>3 <br> Pós Consulta</h6>
+      <p>O acompanhamento do tratamento acontece via WhatsApp, diretamente comigo, e o paciente tem livre acesso para tirar suas dúvidas durante o tratamento.</p>
+      <p>Quando necessário, é agendada a consulta de seguimento para avaliar a evolução e ajustar o tratamento.</p>
+    </div>
+  </section>
+
+  <section>
+    <h5>Como tratar a distância?</h5>
+    <p>Ao final da consulta é enviado ao paciente, via WhatsApp, a prescrição de seu tratamento.</p>
+    <p>O tratamento, na maioria das vezes,é oral (cápsula/sachê) e tópico (tônico/shampoo).</p>
+    <p>O paciente manipula em uma farmácia de manipulação e faz o tratamento em casa.</p>
+    <p>Se houver indicação de procedimento feito em clínica, o mesmo será indicado e orientado (a sugestão de frequência, intensidade, substâncias usadas e tempo). E então o paciente pode buscar em sua região uma clínica especializada que realize o procedimento em questão.</p>
+    <button>Agendar Consulta Online</button>
+  </section>
+
+
+
+
+  <section class="flex column items-center q-gutter-md">
+  <h4>Nosso Estabelecimento</h4>
+  <p>Nosso estabelecimento está localizado no coração da cidade.</p>
+  <iframe 
+      class="q-mx-auto"
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3675.2854430393013!2d-43.109142985410834!3d-22.90284018501375!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9983e534f35a03%3A0x618cde7ed094c742!2sAv.%20Roberto%20Silveira%2C%20215%20-%20Icara%C3%AD%2C%20Niter%C3%B3i%20-%20RJ%2C%2024230-151!5e0!3m2!1spt-BR!2sbr!4v1665930973060!5m2!1spt-BR!2sbr"
+      width="600"
+      height="450"
+      allowfullscreen
+      loading="lazy"
+      referrerpolicy="no-referrer-when-downgrade"
+    />
+  </section>
 </template>
