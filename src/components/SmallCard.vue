bf829637730240b837cdf7737f74890a2ac0e0a9
Arnon <arnonrdp@gmail.com>
2022-10-29 20:09:25 -0300

    Criado componente `SmallCard` para ser reutilizado

---
+++ src/components/SmallCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="card q-ma-sm q-pa-sm">
+    <q-img :src="`src/assets/${imgName}.webp`" width="50%" />
+    <p>{{ label }}</p>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  imgName: { type: String, required: true },
+  label: { type: String, required: true }
+})
+</script>
+
+<style lang="scss" scoped>
+.card {
+  border: 1px solid $primary;
+  border-radius: 24px;
+  max-width: 240px;
+}
+</style>
